8a0321fba0c264f4e36484e94f814dc93ff0150f
roderickli0626 <roderickli0626@gmail.com>
2023-12-04 19:46:02 +0900

    Fix

---
+++ NFC/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(NFC.App_Start.Startup))]
+namespace NFC.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
