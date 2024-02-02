bf0271c61b20ac460e52fc0c22633d5554b6c2c8
roderickli0626 <roderickli0626@gmail.com>
2024-02-02 21:21:21 +0900

    Close gate command

--- NFC/Controller/BasicController.cs
+++ NFC/Controller/BasicController.cs
@@ -332,7 +332,7 @@ namespace NFC.Controller
                     return result;
                 }
 
-                SendCommandToRelay(place);
+                // SendCommandToRelay(place);
                 //Send Notification
                 var hubContext1 = GlobalHost.ConnectionManager.GetHubContext<SignalRHub>();
                 hubContext1.Clients.All.receiveAccessNotification("Accesso in corso");
@@ -440,7 +440,7 @@ namespace NFC.Controller
                     return result;
                 }
 
-                SendCommandToRelay(place);
+                // SendCommandToRelay(place);
                 //Send Notification
                 var hubContext1 = GlobalHost.ConnectionManager.GetHubContext<SignalRHub>();
                 hubContext1.Clients.All.receiveAccessNotification("Accesso in corso");
