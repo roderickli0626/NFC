2d110434bae6cd89abd983fd43874d405f5be405
roderickli0626 <roderickli0626@gmail.com>
2024-02-07 18:52:15 +0900

    Add Log file for Data From Reader

--- NFC/ORBI.aspx.cs
+++ NFC/ORBI.aspx.cs
@@ -2,6 +2,7 @@
 using NFC.Util;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Web;
@@ -14,7 +15,18 @@ namespace NFC
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string testRequest = Request.Url.ToString();
+            string testRequest = Request.Url.AbsoluteUri;
+            // Add Log file with Data From Reader
+            string data = testRequest.Substring(testRequest.IndexOf('?') + 1);
+            data = data.Replace("&", ", ");
+            string logFilePath = Server.MapPath("~/App_Data/log.txt"); // Path to the log file
+            // Write the data to the log file
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine(DateTime.Now.ToString() + " - " + data);
+            }
+
+
             string CMD = Request.Params["cmd"];
             string UID = Request.Params["uid"];
             string PlaceIP = Request.Params["id"];
